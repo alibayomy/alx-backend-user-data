ef661c8d5c40a71f550ef72a5e94aa9419937855
Ali Bayomy <alibayomy12@gmail.com>
2024-05-22 00:20:31 +0300

    app.py finished

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Module of Basic Authentication
+"""
+from api.v1.auth.auth import Auth
+from base64 import b64decode
+from models.user import User
+from typing import TypeVar
+
+
+class BasicAuth(Auth):
+    """Basic authentication class.
+    """
+    pass
